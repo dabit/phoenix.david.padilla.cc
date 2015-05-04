b4106fdcc1c97b519b787eb56231a2954665fcc3
David Padilla <david@easybroker.com>
2015-05-04 10:42:23 -0500

    Record Analytics click events

---
+++ web/static/js/app/ganalytics.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('.archive-link a').bind('click', {'category': 'archive-link'}, sendAnalyticsEvent);
+  $('.more-link a').bind('click', {'category': 'more-link'}, sendAnalyticsEvent);
+
+  function sendAnalyticsEvent(e, category){
+    ga('send', 'event', e.data.category, 'click', $(e.currentTarget).text());
+  }
+});
+
