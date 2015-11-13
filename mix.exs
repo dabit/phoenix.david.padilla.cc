bfc70c9268d0e8e5616dfad0037853e4797f9642
David Padilla <david@easybroker.com>
2015-11-12 23:19:16 -0600

    Upgrade Phoenix to 0.17

--- mix.exs
+++ mix.exs
@@ -31,13 +31,13 @@ defmodule Blog.Mixfile do
   #
   # Type `mix help deps` for examples and options
   defp deps do
-    [{:phoenix, "~> 0.16.0"},
+    [{:phoenix, "~> 0.17.0"},
      {:phoenix_ecto, "~> 0.9.0"},
      {:phoenix_html, "~> 2.0.0"},
      {:earmark, "~> 0.1.8"},
      {:exrm, "~> 0.19.9"},
      {:postgrex, ">= 0.0.0"},
-     {:phoenix_live_reload, "~> 0.5.0", only: :dev},
+     {:phoenix_live_reload, "~> 1.0.0", only: :dev},
      {:comeonin, "~> 0.8"},
      {:cowboy, "~> 1.0"},
      {:timex, "~> 0.19.2"},
