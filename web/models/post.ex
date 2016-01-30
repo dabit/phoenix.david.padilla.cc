c1e46c6987d864edae57b3aca42e50651fbb8724
David Padilla <david@easybroker.com>
2016-01-30 00:08:23 -0600

    Fix model incompatibility

--- web/models/post.ex
+++ web/models/post.ex
@@ -106,7 +106,7 @@ defmodule Blog.Post do
   If `params` are nil, an invalid changeset is returned
   with no validation performed.
   """
-  def changeset(model, params \\ nil) do
+  def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
   end
